30251771679facb148f8f955f7f9faa9271bcd13
Rafiyad Darazat <141349190+rafiyad@users.noreply.github.com>
2023-08-11 22:03:32 +0600

    Spring boot and REST API
    Working on Spring boot, postgres, REST API

---
+++ demoDB/src/test/java/com/college/demoDB/DemoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.college.demoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
